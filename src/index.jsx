01a6f8cd40e6a2559506271d01c6daf40a6be1c3
Hussein Mohammed <zidhussein@gmail.com>
2016-10-29 17:54:10 +0100

    Added MuiTheme

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import App from './components/App'
 import Main from './components/Main'
 import Login from './components/Login'
 
+injectTapEventPlugin()
 
 const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Main} />
-      <Route path="/login" component={Login} />
-    </Route>
-  </Router>
+  <MuiThemeProvider>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Main} />
+        <Route path="/login" component={Login} />
+      </Route>
+    </Router>
+  </MuiThemeProvider>
 )
 
 
