199f37f965a49e2e4267933d38c63c45ba24db01
Arslaan Qadus <azy2k9@gmail.com>
2016-10-29 21:29:10 +0100

    Added links to each button

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import IconButton from 'material-ui/IconButton'
-import BackButton from 'material-ui/svg-icons/navigation/arrow-back'
+
 import NextButton from 'material-ui/svg-icons/image/navigate-next'
 import TextField from 'material-ui/TextField'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 
-const iconButton = {
-  paddingBottom: '50px',
-  float: 'left',
-  clear: 'both',
-}
-
-
+import BackButton from './BackButton'
 
 export default class Register extends React.Component {
   render() {
     return (
       <section className='register'>
-        <IconButton>
-          <BackButton />
-        </IconButton>
+      <BackButton to='/' />
         <TextField hintText="Enter Your Name"/>
         <TextField hintText="Enter Your Phone"/>
         <FloatingActionButton>
